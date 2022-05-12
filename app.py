8a5fc0e116d5441682d00630d79989e9e105aceb
질문KoSeungyu < koeul123@gmail.com>
2022-05-12 10:46:11 +0900

    .decode 추가

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def login():
             'user': id_receive,
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)       # 로그인 24시간 유지
         }                                                                    # jwt 토큰을 발행. 놀이공원 자유입장권과 같은 것. 어떤 사람이 언제까지 입장이 유효하다를 적시해줌.
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
         return jsonify({'result': 'success', 'token': token})
         # 찾지 못하면,
     else:
